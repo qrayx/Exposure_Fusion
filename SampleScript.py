6ebd70339e711a4eb026b510869d208919ba757e
qrayx <40725290+qrayx@users.noreply.github.com>
2018-06-30 15:31:00 -0700

    Add files via upload
    Uploaded the very first file to the project to test out GitHub.

---
+++ SampleScript.py
@@ -0,0 +1,10 @@
+# This is a simple Python script to test out GitHub with
+
+def my_first_method():
+    # This is where my code will go
+
+def partners_first_method():
+    # This is where my collaborator's code will go
+
+my_first_method()
+partners_first_method()
